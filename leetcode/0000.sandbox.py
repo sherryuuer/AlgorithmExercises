ceb0a554c5291ef7f563e6ab27d4e1ca50419997
SallyWang <sherryuuer@gmail.com>
2023-11-08 19:14:52 +0900

    leetcode

--- leetcode/0000.sandbox.py
+++ leetcode/0000.sandbox.py
@@ -1,16 +1,1 @@
-s = "acb"
-
-dp = [[False] * len(s) for _ in range(len(s))]
-print(dp)
-result = 0
-for i in range(len(s)-1, -1, -1): #注意遍历顺序
-    for j in range(i, len(s)):
-        if s[i] == s[j]:
-            if j - i <= 1: #情况一 和 情况二 
-                result += 1
-                dp[i][j] = True
-            elif dp[i+1][j-1]: #情况三
-                result += 1
-                dp[i][j] = True
-print(result)
-print(dp)
+print("test")
