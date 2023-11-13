002abfe552dacf5767a310477ec34dcd93596947
SallyWang <sherryuuer@gmail.com>
2023-11-13 18:56:48 +0900

    merge

--- Algorithm-Sorting/Selection_Sort.py
+++ Algorithm-Sorting/Selection_Sort.py
@@ -12,6 +12,8 @@ def my_selection_sort(arr):
         print(arr)
     return arr
 # I did the same with my gpt buddy.
+# 从第一个数字开始，假定第一个数字是最小的取得它的下标
+# 然后再后面的子序列中寻找最小值的下标和左边的互换
 
 
 sorted_array = my_selection_sort(array)
